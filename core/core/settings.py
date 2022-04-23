45f037f8301de3b216a06fb5daa3989280561bca
TheAliBigdeli <bigdeli.ali3@gmail.com>
2022-04-23 17:19:57 +0400

    added requirements and settings for asgi support

--- core/core/settings.py
+++ core/core/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'channels',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Asgi configs
+ASGI_APPLICATION = "core.asgi.application"
